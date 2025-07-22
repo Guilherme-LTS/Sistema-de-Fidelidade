0627e3740b99729fd3e413c6c665e67b63eb84ec
Guilherme-LTS <glts.snf21@uea.edu.br>
2025-07-22 13:22:43 -0400

    Instaling ChakraProvider and RouterDom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,16 @@
 // frontend/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react'; // 1. Importe o provider
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 2. Envolva o <App /> com o <ChakraProvider> */}
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
