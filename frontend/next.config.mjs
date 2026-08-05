f59dc834dfbdbdb4b0be4911fc3047d7ebfb8c16
Guilherme Lucas Teixeira Silva <99211563+Guilherme-LTS@users.noreply.github.com>
2026-08-05 01:23:26 -0400

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -18,7 +18,9 @@ const nextConfig = {
       "@radix-ui/react-select",
       "@radix-ui/react-tooltip",
       "@radix-ui/react-popover",
-      "@radix-ui/react-[a-z]*",
+      "@radix-ui/react-avatar",
+      "@radix-ui/react-slot",
+      "@radix-ui/react-label",
     ],
   },
   async headers() {
